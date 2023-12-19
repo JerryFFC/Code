2629ea06d1453ecaed5011f75365b843b20711ff
JerryFFC <109345242+JerryFFC@users.noreply.github.com>
2023-12-19 16:49:17 +0000

    d

--- mentions/mentions.py
+++ mentions/mentions.py
@@ -1,7 +1,21 @@
 import pandas as pd
 
-def readMentions(pathFile = r".\Game and mentions_Validated.xlsx"):
-    mentionsDF = pd.read_excel(rf'{pathFile}')
+# def readMentions(pathFile = r".\Game and mentions_Validated.xlsx"):
+#     mentionsDF = pd.read_excel(rf'{pathFile}')
+import os
+import pandas as pd
+
+def readMentions(pathFile="Game and mentions_Validated.xlsx"):
+    # Construct the full path
+    full_path = os.path.join(os.getcwd(), pathFile)
+    print(f"Reading file from: {full_path}")  # For debugging, to see the full path
+
+    # Read the Excel file
+    mentionsDF = pd.read_excel(full_path)
+
+
+
+
     print(mentionsDF)
     targetGameNames = list(mentionsDF['Game name'])
     print(targetGameNames)
